509846e7599db127394bdb16bec36c86b703c5a2
root <root@Khuno>
2021-03-22 21:53:39 -0400

    sending to the cloud

--- src/GitPackage/GitClass.java
+++ src/GitPackage/GitClass.java
@@ -3,7 +3,7 @@ package GitPackage;
 public class GitClass {
 public static void main(String[] args) {
 	System.out.println("original");
-		System.out.println("changes on the cloud");
-
+	System.out.println("changes on the cloud");
+	System.out.println("changes on local computer");
 }
 }
